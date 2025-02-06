06ae9ef2cafc8d79230fcaaea23a1038e5bd448c
Laila Gayden <lgayden@usc.edu>
2025-02-06 06:31:09 +0000

    done

--- split.cpp
+++ split.cpp
@@ -13,11 +13,68 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+// void append(Node*& head, int v); //must also be recursive 
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+//first, step through the in list + determine which values are odd/even
+//once you find out it's odd/even append that value to the corresponding list
+//set that value to NULL in the original list?
+  // Node* oddHead = nullptr;
+  // Node* evenHead = nullptr;
+  // Node* inHead = nullptr; 
+
+  if(in == nullptr){
+    return;
+  }
+
+  Node* temp = in->next;
+  if(in->value % 2 == 0){ //if the value
+    in->next = evens; 
+    evens = in;
+  }
+  else{
+    in->next = odds;
+    odds = in;
+  }
+  in = nullptr; 
+  split(temp, odds, evens);
+
+  //need to do this recursively until I reach NULL 
+  //have to iterate through list until you reach NULL;
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  //odds points to the head of a linked list containing all items
+  //where value is an odd integer 
+
+  //evens points to the head of a linked list containing all items 
+  //where value is an even integer 
+
+
+  
 }
 
 /* If you needed a helper function, write it here */
+// void append(Node*& head, int v){
+  //if(head == NULL){
+    //   Node* newptr = new Node;
+    //   newptr->val = v; 
+    //   newptr->next = NULL;
+  //}
+
+// }
